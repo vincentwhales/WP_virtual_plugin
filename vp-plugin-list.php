97beefdcbcbdbedf860d7732e684c54b90bd4a19
Sergey <compt.sergey@gmail.com>
2018-05-04 15:51:19 +0200

    Updater

--- vp-plugin-list.php
+++ vp-plugin-list.php
@@ -1,10 +1,30 @@
 <?php
 /*
 Plugin Name: Wordpress Virtual Page Plugin List
+Plugin URI: https://github.com/Serget/WP_virtual_plugin
 Description: Plugin returns JSON from get_plugins() function
 Author: Sergey Fesenko
 Version: 1.0
 */
+
+    require_once('updater.php');
+	if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+		$config = array(
+			'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+			'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
+			'api_url' => 'https://api.github.com/repos/Serget/WP_virtual_plugin', // the GitHub API url of your GitHub repo
+			'raw_url' => 'https://raw.github.com/Serget/WP_virtual_plugin/master', // the GitHub raw url of your GitHub repo
+			'github_url' => 'https://github.com/Serget/WP_virtual_plugin', // the GitHub url of your GitHub repo
+			'zip_url' => 'https://github.com/Serget/WP_virtual_plugin/zipball/master', // the zip url of the GitHub repo
+			'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+			'requires' => '3.0', // which version of WordPress does your plugin require?
+			'tested' => '4.9', // which version of WordPress is your plugin tested up to?
+			'readme' => 'README.md', // which file to use as the readme for the version number
+			'access_token' => '', // Access private repositories by authorizing under Appearance > GitHub Updates when this example plugin is installed
+		);
+		new WP_GitHub_Updater($config);
+	}
+
 
 
 /*
