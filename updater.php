5b3936c5793451d652b12a26b2ce645697978332
Sergey <compt.sergey@gmail.com>
2018-05-05 23:27:44 +0200

    check updates every 3 hours

--- updater.php
+++ updater.php
@@ -297,8 +297,8 @@ class WP_GitHub_Updater {
 
 				$github_data = json_decode( $github_data['body'] );
 
-				// refresh every 6 hours
-				set_site_transient( md5($this->config['slug']).'_github_data', $github_data, 60*60*6 );
+				// refresh every 3 hours
+				set_site_transient( md5($this->config['slug']).'_github_data', $github_data, 60*60*3 );
 			}
 
 			// Store the data in this class instance for future calls
